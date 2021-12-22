75cf8dee14a81342f27dabfa237232576ed30bd0
fhaards <you@example.com>
2021-12-22 15:26:53 +0700

    first commit

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>WNA</title>
+    <?php include "stacks-css.php"; ?>
+</head>
+
+<body>
+    <div class="bg-light w-100 h-100vh justify-content-center">
+        <?php include "login-form.php"; ?>
+    </div>
+    <?php include "stacks-js.php"; ?>
+</body>
+
+</html>
